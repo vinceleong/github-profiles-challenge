4a67ff9c63777dc0bc6d7b902925a9288a6a3f3b
vince.leong <vince.leong@simpletruss.com>
2024-08-26 20:25:03 +0800

    .

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,3 +1,3 @@
-export function capitalize(text: string) {
-  return text[0].toUpperCase() + text.slice(1).toLowerCase();
+export function capitalizeddd(text: string) {
+  return "def" + text[0].toUpperCase() + "ac" + text.slice(1).toLowerCase();
 }
